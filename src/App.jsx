2020eaaf97485fe8dbd870d8dc596cd1e112c1c1
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2024-09-17 16:26:52 +0600

    Navbar Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import Navbar from "./components/Navbar"
+import Highlights from "./components/Highlights"
+import Hero from "./components/Hero"
 
 
 const App = () => {
@@ -5,9 +8,11 @@ const App = () => {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <main className="bg-black">
+          <Navbar/>
+          <Hero/>
+          <Highlights/>
+     </main>
     </>
   )
 }
