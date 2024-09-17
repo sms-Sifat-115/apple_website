b22e8636b7b800d634c8ea6a934087aaab176042
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2024-09-18 01:02:12 +0600

    HowItWorks Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Highlights from "./components/Highlights"
 import Hero from "./components/Hero"
 import Model from "./components/Model"
 import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
 
 const App = () => {
 
@@ -15,6 +16,7 @@ const App = () => {
           <Highlights/>
           <Model/>
           <Features/>
+          <HowItWorks/>
      </main>
     </>
   )
